49e5024886820abe8737513c04b4720fc8179955
Hossam Sayed <hsayed@sumerge.com>
2025-07-17 14:16:14 +0300

    Add authorization to user endpoints

--- auth-service/src/main/java/com/edp/auth/controller/UserController.java
+++ auth-service/src/main/java/com/edp/auth/controller/UserController.java
@@ -6,6 +6,7 @@ import com.edp.auth.model.UserUpdateRequestDto;
 import com.edp.auth.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
--- auth-service/src/main/java/com/edp/auth/controller/UserControllerApi.java
+++ auth-service/src/main/java/com/edp/auth/controller/UserControllerApi.java
@@ -4,6 +4,7 @@ import com.edp.auth.model.UserRegisterRequestDto;
 import com.edp.auth.model.UserResponseDto;
 import com.edp.auth.model.UserUpdateRequestDto;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
@@ -12,19 +13,23 @@ import java.util.List;
 @RequestMapping("/api/users")
 public interface UserControllerApi {
 
-    // TODO: Add Admin role check or remove according to desired behavior
     @PostMapping
+    @PreAuthorize("hasRole('ADMIN')")
     ResponseEntity<Void> createUser(@RequestBody UserRegisterRequestDto userRegisterRequestDto, UriComponentsBuilder uriBuilder);
 
     @GetMapping("/{id}")
+    @PreAuthorize("hasRole('ADMIN') or #id == T(com.edp.auth.data.entity.AppUser).cast(authentication.principal).id")
     ResponseEntity<UserResponseDto> getUser(@PathVariable Long id);
 
     @GetMapping
+    @PreAuthorize("hasAnyRole('ADMIN', 'USER')")
     ResponseEntity<List<UserResponseDto>> getAllUsers();
 
     @PutMapping("/{id}")
+    @PreAuthorize("hasRole('ADMIN') or #id == T(com.edp.auth.data.entity.AppUser).cast(authentication.principal).id")
     ResponseEntity<Void> updateUser(@PathVariable Long id, @RequestBody UserUpdateRequestDto userUpdateRequestDto);
 
     @DeleteMapping("/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
     ResponseEntity<Void> deleteUser(@PathVariable Long id);
 }
