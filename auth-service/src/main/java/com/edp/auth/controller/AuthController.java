3341bcd863f3209649a3c807963374bec25fc594
Ahmed Medhat Zahra <ahmedmedhatzahra@gmail.com>
2025-07-27 04:26:58 +0300

    Add CORS config to Authcontroller

--- auth-service/src/main/java/com/edp/auth/controller/AuthController.java
+++ auth-service/src/main/java/com/edp/auth/controller/AuthController.java
@@ -12,14 +12,12 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/api/auth")
 @RequiredArgsConstructor
