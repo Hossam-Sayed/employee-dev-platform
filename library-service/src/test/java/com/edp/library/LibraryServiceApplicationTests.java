57c1a3a0bfacbe27e30777af9bc6a22ab98cff26
Hossam Sayed <hsayed@sumerge.com>
2025-08-05 10:33:07 +0300

    Start library service implementation

---
+++ library-service/src/test/java/com/edp/library/LibraryServiceApplicationTests.java
@@ -0,0 +1,12 @@
+package com.edp.library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
